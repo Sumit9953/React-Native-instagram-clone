c366dd69ab93ed194bdae4a568d49f98042c3045
sumit <sumitsantosh555@gmail.com>
2023-03-24 14:33:03 +0530

    React form hook implement compelte

--- src/screen/EditProfileScreen/EditProfileScreen.tsx
+++ src/screen/EditProfileScreen/EditProfileScreen.tsx
@@ -1,14 +1,17 @@
 import {View, Text, StyleSheet, Image, TextInput} from 'react-native';
-import {useForm, Controller , Control} from 'react-hook-form';
+import {useForm, Controller, Control} from 'react-hook-form';
 import user from '../../assets/data/user.json';
 import colors from '../../theme/color';
 import fonts from '../../theme/fonts';
-import { IUser } from '../../types/models';
+import {IUser} from '../../types/models';
 
-type IEditableUserField = 'name' | 'username' | 'website' |'bio'
-type IEditableUser = Pick<IUser , IEditableUserField >
+const URL_REGEX =
+  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?\/?$/i;
+
+type IEditableUserField = 'name' | 'username' | 'website' | 'bio';
+type IEditableUser = Pick<IUser, IEditableUserField>;
 interface ICustomInput {
-  control: Control<IEditableUser , Object>;
+  control: Control<IEditableUser, Object>;
   label: string;
   name: IEditableUserField;
   multiline?: boolean;
@@ -20,48 +23,103 @@ const CustomInput = ({
   control,
   name,
   multiline = false,
-  rules = {}
+  rules = {},
 }: ICustomInput) => (
   <Controller
     control={control}
     name={name}
     rules={rules}
-    render={({field: {onChange, value, onBlur} , fieldState: {error}}) => {
-    
-    return ( 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>{label}</Text>
-        <View style={{flex: 1}}>
-        <TextInput
-          value={value}
-          onChangeText={onChange}
-          onBlur={onBlur}
-          style={[styles.input , {borderColor: error ? colors.red : colors.border}]}
-          placeholder={label}
-          multiline={multiline}
-        />
-        {error && <Text style={{color: colors.red}}>{error.type}</Text>}
+    render={({field: {onChange, value, onBlur}, fieldState: {error}}) => {
+      return (
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>{label}</Text>
+          <View style={{flex: 1}}>
+            <TextInput
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              style={[
+                styles.input,
+                {borderColor: error ? colors.red : colors.border},
+              ]}
+              placeholder={label}
+              multiline={multiline}
+            />
+            {error && (
+              <Text style={{color: colors.red}}>
+                {error.message || 'Error'}
+              </Text>
+            )}
+          </View>
         </View>
-      </View>
-    )}}
+      );
+    }}
   />
 );
 
 const EditProfileScreen = () => {
-  const {control, handleSubmit , formState: {errors} } = useForm<IEditableUser>();
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<IEditableUser>({
+    defaultValues: {
+        name: user.name,
+        username: user.username,
+        website: '',
+        bio: user.bio
+    }
+  });
 
-  const onSubmit = (data : IEditableUser) => {
-    console.log('submit' , data);
+  const onSubmit = (data: IEditableUser) => {
+    console.log('submit', data);
   };
   return (
     <View style={styles.page}>
       <Image source={{uri: user.image}} style={styles.avatar} />
       <Text style={styles.textButton}>Change profile photo</Text>
 
-      <CustomInput name="name" control={control} rules={{required : true}}  label="Name" />
-      <CustomInput name="username" control={control} rules={{required: true}} label="Username" />
-      <CustomInput name="website" control={control} rules={{required: true}} label="Website" />
-      <CustomInput name="bio" control={control} rules={{required: true}} label="Bio" multiline />
+      <CustomInput
+        name="name"
+        control={control}
+        rules={{required: 'Name is required'}}
+        label="Name"
+      />
+      <CustomInput
+        name="username"
+        control={control}
+        rules={{
+          required: 'Username is required',
+          minLength: {
+            value: 3,
+            message: 'Username should be more than 3 character',
+          },
+        }}
+        label="Username"
+      />
+      <CustomInput
+        name="website"
+        control={control}
+        rules={{
+          pattern: {
+            value: URL_REGEX,
+            message: 'Invalid url',
+          },
+        }}
+        label="Website"
+      />
+      <CustomInput
+        name="bio"
+        control={control}
+        rules={{
+          maxLength: {
+            value: 200,
+            message: 'Bio should be less than 200 character',
+          },
+        }}
+        label="Bio"
+        multiline
+      />
 
       <Text onPress={handleSubmit(onSubmit)} style={styles.textButton}>
         Submit
